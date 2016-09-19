9a6c7a328f10f556633a1618cf718a39054c93f2
alexsiqingliu <uilxela7@gmail.com>
2016-09-19 16:24:24 -0400

    initial edits

---
+++ BSTTest.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Iterator;
+
+public class BSTTest
+{
+    public static void main(String[] args)
+    {
+	BinarySearchTree<Integer> t = new BinarySearchTree<Integer>();
+
+	String test1 = "a 10 a 5 a 15 a 1 a 20 a 2 a 17";
+
+	Scanner in = new Scanner(System.in);
+	//Scanner in = new Scanner(System.in);
+
+	in 
+	while (in.hasNext())
+	    {
+		String command = in.next();
+		if (command.toLowerCase().startsWith("a"))
+		    {
+			int i = in.nextInt();
+			t.add(i);
+		    }
+		else if (command.toLowerCase().startsWith("r"))
+		    {
+			int i = in.nextInt();
+			t.remove(i);
+		    }
+		else
+		    {
+			in.nextLine();
+		    }
+		t.validate();
+		
+		Iterator<Integer> i = t.inorderIterator();
+		while (i.hasNext())
+		    {
+			System.out.print(i.next() + " ");
+		    }
+		System.out.println();
+	    }
+	System.out.println(t + "alsdjf");
+    }
+}
